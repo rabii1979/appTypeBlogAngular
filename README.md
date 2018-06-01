f1f84ba3fc7ccd6eea1fb5f80a8f945d12886602
rabii1979 <benaata_rabii@hotmail.fr>
2018-06-01 12:23:52 +0200

    second commit

--- README.md
+++ README.md
@@ -1,27 +1,11 @@
 # AppTypeBlog
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.5.
+Cette application Web Front End avec Angular qui va afficher les posts du blog, et chaque post aura un bouton permettant de "love it" ou de "don't love it"
 
 ## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Exécutez `ng serve` pour un serveur de dev. Accédez à `http: // localhost: 4200 /`. L'application sera automatiquement rechargée si vous modifiez l'un des fichiers source.
 
-## Code scaffolding
+## Installation les dépendances :
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+npm install
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-    titre = 'type blog';
+    titre = 'type  blog';
 
     posts = [
         {
-            title : "Mon premier post" ,
-            content: "Quid enim tam absurdum quam delectari multis inanimis rebus, ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit, non admodum delectari? Nihil est enim remuneratione benevolentiae, nihil vicissitudine studiorum officiorumque iucundius.",
-            loveIts : 2,
+            title: 'Mon premier post',
+            content: 'Quid enim tam absurdum quam delectari multis inanimis rebus, ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit, non admodum delectari? Nihil est enim remuneratione benevolentiae, nihil vicissitudine studiorum officiorumque iucundius.',
+            loveIts: 2,
             created_at: new Date()
         },
         {
-            title : "Mon deuxième  post" ,
-            content: "Quid enim tam absurdum quam delectari multis inanimis rebus, ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit, non admodum delectari? Nihil est enim remuneratione benevolentiae, nihil vicissitudine studiorum officiorumque iucundius.",
-            loveIts : -3,
+            title: 'Mon deuxième  post',
+            content: 'Quid enim tam absurdum quam delectari multis inanimis rebus, ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit, non admodum delectari? Nihil est enim remuneratione benevolentiae, nihil vicissitudine studiorum officiorumque iucundius.',
+            loveIts: -3,
             created_at: new Date()
         },
         {
-            title : "Encore un  post" ,
-            content: "Quid enim tam absurdum quam delectari multis inanimis rebus, ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit, non admodum delectari? Nihil est enim remuneratione benevolentiae, nihil vicissitudine studiorum officiorumque iucundius.",
-            loveIts : 0,
+            title: 'Encore un  post',
+            content: 'Quid enim tam absurdum quam delectari multis inanimis rebus, ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit, non admodum delectari? Nihil est enim remuneratione benevolentiae, nihil vicissitudine studiorum officiorumque iucundius.',
+            loveIts: 0,
             created_at: new Date()
         }
-    ]
+    ];
 }
