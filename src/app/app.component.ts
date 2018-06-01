84ffd56257fec020b55c9121b37a8d9298b2d130
rabii1979 <benaata_rabii@hotmail.fr>
2018-06-01 11:52:54 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+    titre = 'type blog';
+
+    posts = [
+        {
+            title : "Mon premier post" ,
+            content: "Quid enim tam absurdum quam delectari multis inanimis rebus, ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit, non admodum delectari? Nihil est enim remuneratione benevolentiae, nihil vicissitudine studiorum officiorumque iucundius.",
+            loveIts : 2,
+            created_at: new Date()
+        },
+        {
+            title : "Mon deuxième  post" ,
+            content: "Quid enim tam absurdum quam delectari multis inanimis rebus, ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit, non admodum delectari? Nihil est enim remuneratione benevolentiae, nihil vicissitudine studiorum officiorumque iucundius.",
+            loveIts : -3,
+            created_at: new Date()
+        },
+        {
+            title : "Encore un  post" ,
+            content: "Quid enim tam absurdum quam delectari multis inanimis rebus, ut honore, ut gloria, ut aedificio, ut vestitu cultuque corporis, animante virtute praedito, eo qui vel amare vel, ut ita dicam, redamare possit, non admodum delectari? Nihil est enim remuneratione benevolentiae, nihil vicissitudine studiorum officiorumque iucundius.",
+            loveIts : 0,
+            created_at: new Date()
+        }
+    ]
 }
